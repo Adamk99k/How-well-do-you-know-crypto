42a519d5d0ec77ea21e077e62b8846498fed7b1a
Adam <Adamkhan0531@hotmail.com>
2022-06-27 23:24:50 +0000

    Add Javascript basic methods to acsess the DOM and asign text values to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// Method that runs a task when user has submited there answer
+document.getElementById("q1button").onclick = function() {
+    var q1Answer = document.getElementById("q1text").value;
+    console.log("This was user's answer: " +q1Answer)
+}
+
+document.getElementById("q2button").onclick = function() {
+    var q2Answer = document.getElementById("q2text").value;
+    console.log("This was user's answer: " +q2Answer)
+}
+
+document.getElementById("q3button").onclick = function() {
+    var q3Answer = document.getElementById("q3text").value;
+    console.log("This was user's answer: " +q3Answer)
+}
+
+document.getElementById("q4button").onclick = function() {
+    var q4Answer = document.getElementById("q4text").value;
+    console.log("This was user's answer: " +q4Answer)
+}
+
+document.getElementById("q5button").onclick = function() {
+    var q5Answer = document.getElementById("q5text").value;
+    console.log("This was user's answer: " +q5Answer)
+}
+
+document.getElementById("q6button").onclick = function() {
+    var q6Answer = document.getElementById("q6text").value;
+    console.log("This was user's answer: " +q6Answer)
+}
