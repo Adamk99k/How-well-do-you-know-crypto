41a5d2979702fb37c0608853f57243cc965194a7
Adam <Adamkhan0531@hotmail.com>
2022-06-28 02:55:15 +0000

    Add elements that make up Score page and methods to maniplulate the DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ document.getElementById("q1button").onclick = function() {
         document.getElementById("questionOne").style.background = 'Green';
         document.querySelector("h2").innerHTML = "Correct. <br> Bitcoin is the largest coin by marketcap.";
         console.log("Answer was correct ! ! !")
+        userScore++
     }
 }
 
@@ -25,6 +26,7 @@ document.getElementById("q2button").onclick = function() {
         document.getElementById("questionTwo").style.background = 'Green';
         document.querySelectorAll("h2")[1].innerHTML = "Correct. <br> NameCoin was the first altcoin to be created.";
         console.log("Answer was correct ! ! !")
+        userScore++
     }
 }
 
@@ -39,6 +41,7 @@ document.getElementById("q3button").onclick = function() {
         document.getElementById("questionThree").style.background = 'Green';
         document.querySelectorAll("h2")[2].innerHTML = "Correct. <br> Tether (USDT) is the biggest stable coin to date.";
         console.log("Answer was correct ! ! !")
+        userScore++
     }
 }
 // Question Four
@@ -52,6 +55,7 @@ document.getElementById("q4button").onclick = function() {
         document.getElementById("questionFour").style.background = 'Green';
         document.querySelectorAll("h2")[3].innerHTML = "Correct. <br> (ETH) is the biggest altcoin to date";
         console.log("Answer was correct ! ! !")
+        userScore++
     }
 }
 
@@ -66,6 +70,7 @@ document.getElementById("q5button").onclick = function() {
         document.getElementById("questionFive").style.background = 'Green';
         document.querySelectorAll("h2")[4].innerHTML = "Correct. <br> name was the founder of BitCoin.";
         console.log("Answer was correct ! ! !")
+        userScore++
     }
 }
 
@@ -80,5 +85,15 @@ document.getElementById("q6button").onclick = function() {
         document.getElementById("questionSix").style.background = 'Green';
         document.querySelectorAll("h2")[5].innerHTML = "Correct. <br> This was the year it was made (PLACEHOLDER)";
         console.log("Answer was correct ! ! !")
+        userScore++
     }
+}
+
+// Check score button pull from DOM and maniplate to hide container and add new content
+document.getElementById("checkScore").onclick = function() {
+    document.getElementById("container").style.display = "none";
+    document.getElementById("checkScore").style.display = "none";
+    document.getElementById("header-text").innerHTML = "Quiz Ended...";
+    document.getElementById("scorePageContainer").style.display = "block";
+    document.getElementById("finalScore").innerHTML = "You scored a total of: " + userScore + " Out of 6 Questions."
 }
