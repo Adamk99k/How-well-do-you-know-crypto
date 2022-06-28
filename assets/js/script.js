8a35ef3eb40b66dd887cc647171ad2620a322f48
Adam <Adamkhan0531@hotmail.com>
2022-06-28 03:56:36 +0000

    Add more detailed infomation for each question

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,11 @@ document.getElementById("q1button").onclick = function() {
     var q1Answer = document.getElementById("q1text").value.toLowerCase();
     if ( q1Answer != "bitcoin") {
         document.getElementById("questionOne").style.background = "Red";
-        document.querySelector("h2").innerHTML = "Wrong answer ! ("+q1Answer+ ") Is not correct.";
-        console.log("Users input was " + q1Answer, "This was Incorect");
+        document.querySelector("h2").innerHTML = "Wrong answer ! ("+q1Answer+ ") Is not correct. Please try again or skip on to the next Question.";
+        console.log("Users input was " + q1Answer, "This was Incorect.");
     } else {
         document.getElementById("questionOne").style.background = 'Green';
-        document.querySelector("h2").innerHTML = "Correct. <br> Bitcoin is the largest coin by marketcap.";
+        document.querySelector("h2").innerHTML = "Correct. <br> Bitcoin is a decentralized digital currency that is based on cryptography. As such, it can operate without the need of a central authority like a central bank or a company. Its by far the largest coin by MarketCap.";
         console.log("Answer was correct ! ! !")
         userScore++
     }
@@ -20,11 +20,11 @@ document.getElementById("q2button").onclick = function() {
     var q2Answer = document.getElementById("q2text").value.toLowerCase();
     if ( q2Answer != "namecoin") {
         document.getElementById("questionTwo").style.background = "Red";
-        document.querySelectorAll("h2")[1].innerHTML = "Wrong answer ! ("+q2Answer+ ") Is not correct.";
+        document.querySelectorAll("h2")[1].innerHTML = "Wrong answer ! ("+q2Answer+ ") Is not correct. Please try again or skip on to the next Question.";
         console.log("Users input was " + q2Answer, "This was Incorect");
     } else {
         document.getElementById("questionTwo").style.background = 'Green';
-        document.querySelectorAll("h2")[1].innerHTML = "Correct. <br> NameCoin was the first altcoin to be created.";
+        document.querySelectorAll("h2")[1].innerHTML = "Correct. <br> NameCoin was the first altcoin to be created. A pseudo-anonymous founder by the name of “Vince” created Namecoin in 2011. Since then, Vince has disappeared, but a core development team has kept the project alive.";
         console.log("Answer was correct ! ! !")
         userScore++
     }
@@ -35,11 +35,11 @@ document.getElementById("q3button").onclick = function() {
     var q3Answer = document.getElementById("q3text").value.toLowerCase();
     if ( q3Answer != "usdt") {
         document.getElementById("questionThree").style.background = "Red";
-        document.querySelectorAll("h2")[2].innerHTML = "Wrong answer ! ("+q3Answer+ ") Is not correct.";
+        document.querySelectorAll("h2")[2].innerHTML = "Wrong answer ! ("+q3Answer+ ") Is not correct. Please try again or skip on to the next Question.";
         console.log("Users input was " + q3Answer, "This was Incorect");
     } else {
         document.getElementById("questionThree").style.background = 'Green';
-        document.querySelectorAll("h2")[2].innerHTML = "Correct. <br> Tether (USDT) is the biggest stable coin to date.";
+        document.querySelectorAll("h2")[2].innerHTML = "Correct. <br> Tether (USDT) is a stablecoin pegged to the US Dollar. A stablecoin is a type of cryptocurrency whose value is pegged to another fiat currency like the US Dollar or to a commodity like Gold.";
         console.log("Answer was correct ! ! !")
         userScore++
     }
@@ -49,11 +49,11 @@ document.getElementById("q4button").onclick = function() {
     var q4Answer = document.getElementById("q4text").value.toLowerCase();
     if ( q4Answer != "eth") {
         document.getElementById("questionFour").style.background = "Red";
-        document.querySelectorAll("h2")[3].innerHTML = "Wrong answer ! ("+q4Answer+ ") Is not correct.";
+        document.querySelectorAll("h2")[3].innerHTML = "Wrong answer ! ("+q4Answer+ ") Is not correct. Please try again or skip on to the next Question.";
         console.log("Users input was " + q4Answer, "This was Incorect");
     } else {
         document.getElementById("questionFour").style.background = 'Green';
-        document.querySelectorAll("h2")[3].innerHTML = "Correct. <br> (ETH) is the biggest altcoin to date";
+        document.querySelectorAll("h2")[3].innerHTML = "Correct. <br> (ETH) is the biggest altcoin to date. The founding team of Ethereum are known individuals which includes Vitalik Buterin, Mihai Alisie, Anthony Di lorio, Charles Hoskinson, Amir Chetrit, Joseph Lubin, Gavin Wood, and Jeffrey Wilke.";
         console.log("Answer was correct ! ! !")
         userScore++
     }
@@ -64,11 +64,11 @@ document.getElementById("q5button").onclick = function() {
     var q5Answer = document.getElementById("q5text").value.toLowerCase();
     if ( q5Answer != "satoshi nakamoto") {
         document.getElementById("questionFive").style.background = "Red";
-        document.querySelectorAll("h2")[4].innerHTML = "Wrong answer ! ("+q5Answer+ ") Is not correct.";
+        document.querySelectorAll("h2")[4].innerHTML = "Wrong answer ! ("+q5Answer+ ") Is not correct. Please try again or skip on to the next Question.";
         console.log("Users input was " + q5Answer, "This was Incorect");
     } else {
         document.getElementById("questionFive").style.background = 'Green';
-        document.querySelectorAll("h2")[4].innerHTML = "Correct. <br> name was the founder of BitCoin.";
+        document.querySelectorAll("h2")[4].innerHTML = "Correct. <br> The creator is an unknown individual or group that goes by the name Satoshi Nakamoto with the idea of an electronic peer-to-peer cash system as it is written in the BitCoin whitepaper.";
         console.log("Answer was correct ! ! !")
         userScore++
     }
@@ -79,11 +79,11 @@ document.getElementById("q6button").onclick = function() {
     var q6Answer = document.getElementById("q6text").value.toLowerCase();
     if ( q6Answer != "2009") {
         document.getElementById("questionSix").style.background = "Red";
-        document.querySelectorAll("h2")[5].innerHTML = "Wrong answer ! ("+q6Answer+ ") Is not correct.";
+        document.querySelectorAll("h2")[5].innerHTML = "Wrong answer ! ("+q6Answer+ ") Is not correct. Please try again or skip on to the next Question.";
         console.log("Users input was " + q6Answer, "This was Incorect");
     } else {
         document.getElementById("questionSix").style.background = 'Green';
-        document.querySelectorAll("h2")[5].innerHTML = "Correct. <br> This was the year it was made (PLACEHOLDER)";
+        document.querySelectorAll("h2")[5].innerHTML = "Correct. <br> Satoshi Nakamoto, the creator of Bitcoin, mined the first Bitcoin block on 3rd January 2009 and started the P2P digital cash system.";
         console.log("Answer was correct ! ! !")
         userScore++
     }
